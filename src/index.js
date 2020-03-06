7af49567a9530dde0d7a61d551b5c6b0a74b696c
mlazzell17@gmail.com <MicheSi>
2020-03-06 16:56:40 -0700

    installed dependencies and set up file structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+// component imports
+import App from './App';
+
+// style imports
+import './index.css';
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
