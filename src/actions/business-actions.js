65be59b463b65cbafc3e17a1c8578124cff7e9fb
mlazzell17@gmail.com <MicheSi>
2020-03-11 11:01:51 -0700

    created get, put and delete actions for bus & vol

--- src/actions/business-actions.js
+++ src/actions/business-actions.js
@@ -1,12 +1,28 @@
 import axios from 'axios';
+import axiosWithAuth from '../utils/axiosWithAuth';
 // register actions
 export const POST_BREGISTER_START = 'POST_BREGISTER_START';
 export const POST_BREGISTER_SUCCESS = 'POST_BREGISTER_SUCCESS';
 export const POST_BREGISTER_FAIL = 'POST_BREGISTER_FAIL';
-// log in actions
+// login actions
 export const POST_BLOGIN_START = 'POST_BLOGIN_START';
 export const POST_BLOGIN_SUCCESS = 'POST_BLOGIN_SUCCESS';
 export const POST_BLOGIN_FAIL = 'POST_BLOGIN_FAIL';
+// read actions
+export const GET_ALLBUSINESS_START = 'GET_ALLBUSINESS_START';
+export const GET_ALLBUSINESS_SUCCESS = 'GET_ALLBUSINESS_SUCCESS';
+export const GET_ALLBUSINESS_FAIL = 'GET_ALLBUSINESS_FAIL';
+export const GET_BUSBYID_START = 'GET_BUSBYID_START';
+export const GET_BUSBYID_SUCCESS = 'GET_BUSBYID_SUCCESS';
+export const GET_BUSBYID_FAIL = 'GET_BUSBYID_FAIL';
+// edit actions
+export const PUT_BUSINESS_START = 'PUT_BUSINESS_START';
+export const PUT_BUSINESS_SUCCESS = 'PUT_BUSINESS_SUCCESS';
+export const PUT_BUSINESS_FAIL = 'PUT_BUSINESS_FAIL';
+// delete actions
+export const REMOVE_BUSINESS_START = 'REMOVE_BUSINESS_START';
+export const REMOVE_BUSINESS_SUCCESS = 'REMOVE_BUSINESS_SUCCESS';
+export const REMOVE_BUSINESS_FAIL = 'REMOVE_BUSINESS_FAIL';
 
 export const registerBusiness = (props) => dispatch => {
     dispatch({type: POST_BREGISTER_START});
@@ -28,4 +44,49 @@ export const loginBusiness = (props) => dispatch => {
     })
     .catch(err =>
         dispatch({type: POST_BLOGIN_FAIL, payload: err}))
+}
+
+export const getAllBus = (props) => dispatch => {
+    dispatch({type: GET_ALLBUSINESS_START});
+    axiosWithAuth()
+    .get('/users/business')
+    .then(res => {
+        dispatch({type: GET_ALLBUSINESS_SUCCESS, payload: res.data})
+    })
+    .catch(err =>
+        dispatch({type: GET_ALLBUSINESS_FAIL, payload: err}))
+}
+
+export const getBusById = (props) => dispatch => {
+    dispatch({type: GET_BUSBYID_START});
+    axiosWithAuth()
+    .get(`/users/business/${id}`)
+    .then(res => {
+        dispatch({type: GET_BUSBYID_SUCCESS, payload: res.data})
+    })
+    .catch(err =>
+        dispatch({type: GET_BUSBYID_FAIL, payload: err}))
+}
+
+export const editBusiness = (props) => dispatch => {
+    dispatch({type: PUT_BUSINESS_START});
+    axiosWithAuth()
+    .put(`/users/business/${id}`, business)
+    .then(res => {
+        console.log('edit', res)
+        dispatch({type: PUT_BUSINESS_SUCCESS, payload: res.data})
+    })
+    .catch(err =>
+        dispatch({type: PUT_BUSINESS_FAIL, payload: err}))
+}
+
+export const deleteBusiness = (props) => dispatch => {
+    dispatch({type: REMOVE_BUSINESS_START});
+    axiosWithAuth()
+    .delete(`/users/business/${id}`, business)
+    .then(res => {
+        dispatch({type: REMOVE_BUSINESS_SUCCESS, payload: res})
+    })
+    .catch(err =>
+        dispatch({type: REMOVE_BUSINESS_FAIL, payload: err}))
 }
--- src/actions/volunteer-actions.js
+++ src/actions/volunteer-actions.js
@@ -1,16 +1,90 @@
 import axios from 'axios';
-
+// register actions
 export const POST_VREGISTER_START = 'POST_VREGISTER_START';
 export const POST_VREGISTER_SUCCESS = 'POST_VREGISTER_SUCCESS';
 export const POST_VREGISTER_FAIL = 'POST_VREGISTER_FAIL';
+// login actions
+export const POST_VLOGIN_START = 'POST_VLOGIN_START';
+export const POST_VLOGIN_SUCCESS = 'POST_VLOGIN_SUCCESS';
+export const POST_VLOGIN_FAIL = 'POST_VLOGIN_FAIL';
+// read actions
+export const GET_ALLVOLUNTEERS_START = 'GET_ALLVOLUNTEERS_START';
+export const GET_ALLVOLUNTEERS_SUCCESS = 'GET_ALLVOLUNTEERS_SUCCESS';
+export const GET_ALLVOLUNTEERS_FAIL = 'GET_ALLVOLUNTEERS_FAIL';
+export const GET_VOLBYID_START = 'GET_VOLBYID_START';
+export const GET_VOLBYID_SUCCESS = 'GET_VOLBYID_SUCCESS';
+export const GET_VOLBYID_FAIL = 'GET_VOLBYID_FAIL';
+// edit actions
+export const PUT_VOLUNTEER_START = 'PUT_VOLUNTEER_START';
+export const PUT_VOLUNTEER_SUCCESS = 'PUT_VOLUNTEER_SUCCESS';
+export const PUT_VOLUNTEER_FAIL = 'PUT_VOLUNTEER_FAIL';
+// delete actions
+export const REMOVE_VOLUNTEER_START = 'REMOVE_VOLUNTEER_START';
+export const REMOVE_VOLUNTEER_SUCCESS = 'REMOVE_VOLUNTEER_SUCCESS';
+export const REMOVE_VOLUNTEER_FAIL = 'REMOVE_VOLUNTEER_FAIL';
 
 export const registerVolunteer = (props) => dispatch => {
     dispatch({type: POST_VREGISTER_START});
     axios
-    .post()
+    .post('https://bw-replate2.herokuapp.com/api/auth/volunteer/register')
     .then(res => {
         dispatch({type: POST_VREGISTER_SUCCESS, payload: res})
     })
     .catch(err =>
         dispatch({type: POST_VREGISTER_FAIL, payload: err}))
+}
+
+export const loginVolunteer = (props) => dispatch => {
+    dispatch({type: POST_VLOGIN_START});
+    axios
+    .post('https://bw-replate2.herokuapp.com/api/auth/volunteer/login')
+    .then(res => {
+        dispatch({type: POST_VLOGIN_SUCCESS, payload: res})
+    })
+    .catch(err =>
+        dispatch({type: POST_VLOGIN_FAIL, payload: err}))
+}
+export const getAllVol = (props) => dispatch => {
+    dispatch({type: GET_ALLVOLUNTEERS_START});
+    axiosWithAuth()
+    .get('/users/volunteer')
+    .then(res => {
+        dispatch({type: GET_ALLVOLUNTEERS_SUCCESS, payload: res.data})
+    })
+    .catch(err =>
+        dispatch({type: GET_ALLVOLUNTEERS_FAIL, payload: err}))
+}
+
+export const getVolById = (props) => dispatch => {
+    dispatch({type: GET_VOLBYID_START});
+    axiosWithAuth()
+    .get(`/users/volunteer/${id}`)
+    .then(res => {
+        dispatch({type: GET_VOLBYID_SUCCESS, payload: res.data})
+    })
+    .catch(err =>
+        dispatch({type: GET_VOLBYID_FAIL, payload: err}))
+}
+
+export const editVolunteer = (props) => dispatch => {
+    dispatch({type: PUT_VOLUNTEER_START});
+    axiosWithAuth()
+    .put(`/users/volunteer/${id}`, volunteer)
+    .then(res => {
+        console.log('edit', res)
+        dispatch({type: PUT_VOLUNTEER_SUCCESS, payload: res.data})
+    })
+    .catch(err =>
+        dispatch({type: PUT_VOLUNTEER_FAIL, payload: err}))
+}
+
+export const deleteVolunteer = (props) => dispatch => {
+    dispatch({type: REMOVE_VOLUNTEER_START});
+    axiosWithAuth()
+    .delete(`/users/volunteer/${id}`, volunteer)
+    .then(res => {
+        dispatch({type: REMOVE_VOLUNTEER_SUCCESS, payload: res})
+    })
+    .catch(err =>
+        dispatch({type: REMOVE_VOLUNTEER_FAIL, payload: err}))
 }
